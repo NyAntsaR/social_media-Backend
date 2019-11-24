1bef86eba2fb77823a3e00e749e12c4cbcfd6a27
Ny Antsa Rajaonarison <nyantsa@Nys-MacBook-Pro.local>
2019-11-24 11:01:21 -0800

    Add user schema in models folder

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    hashed_password: {
+        type: String,
+        required: true
+    },
+    salt: String,
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: Date
+})
+
+module.exports = mongoose.model("User", userSchema);
