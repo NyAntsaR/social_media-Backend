7ecb9bd701aa71647d6fd77e65b0b53858dc1d73
Ny Antsa Rajaonarison <nyantsa@Nys-MacBook-Pro.local>
2019-11-24 11:27:06 -0800

    Encrypting password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const uuidv1 = require("uuid/v1");
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -21,6 +23,35 @@ const userSchema = new mongoose.Schema({
         default: Date.now
     },
     updated: Date
+});
+
+// virtual fields
+userSchema.virtual( 'password' )
+.set( function(password) {
+    // create temporary variable called_password
+    this._password = password;
+    // generate a unique timestamp using the universal unique Identifier package
+    this.salt = uuidv1();
+    // encrypt password
+    this.hashed_password = this.encryptPassword(password);
 })
+.get( function() {
+    return this._password;
+})
+
+// methods to encrypt the password
+userSchema.methods = {
+    encryptPassword: function( password ) {
+        if ( !password ) return "";
+        try {
+            return crypto
+                    .createHmac( 'sha1', this.salt)
+                    .update(password)
+                    .digest('hex');
+        } catch (err) {
+            return "";
+        }
+    }
+}
 
 module.exports = mongoose.model("User", userSchema);
