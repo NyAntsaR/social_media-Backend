1c459d13375ca630a9a23d8f881dd47ca36317c0
andy ratsirarson <andy.ratsirarson@andys-air.lan>
2019-12-03 14:33:34 -0800

    Load single post in state

--- controllers/post.js
+++ controllers/post.js
@@ -117,3 +117,7 @@ exports.photo = (req, res, next) => {
     res.set("Content-Type", req.post.photo.contentType);
     return res.send(req.post.photo.data);
 }
+
+exports.singlePost = (req, res, next) => {
+    return res.json(req.post)
+}
