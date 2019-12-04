846441b155d6b9fd7d52bdf49a52106d3cf554e1
andy ratsirarson <andy.ratsirarson@andys-air.lan>
2019-12-03 18:05:41 -0800

    Implement comment/ uncomment

--- controllers/post.js
+++ controllers/post.js
@@ -6,6 +6,8 @@ const _ = require("lodash");
 exports.postById = (req, res, next, id) => {
     Post.findById(id)
         .populate("postedBy", "_id name")
+        .populate("comments.postedBy", "_id name")
+        .populate("postedBy", "_id name")
         .exec((err, post) => {
             if (err || !post) {
                 return res.status(400).json({
@@ -20,6 +22,8 @@ exports.postById = (req, res, next, id) => {
 exports.getPosts = (req, res) => {
     const posts = Post.find()
         .populate("postedBy", "_id name")
+        .populate("comments", "text created")
+        .populate("comments.postedBy", "_id name")
         .select("_id title body created likes")
         .sort({ created: -1 })
         .then(posts => {
@@ -84,20 +88,6 @@ exports.isPoster = (req, res, next) => {
     }
     next();
 };
-
-// exports.updatePost = (req, res, next) => {
-//     let post = req.post;
-//     post = _.extend(post, req.body);
-//     post.updated = Date.now();
-//     post.save(err => {
-//         if (err) {
-//             return res.status(400).json({
-//                 error: err
-//             });
-//         }
-//         res.json(post);
-//     });
-// };
 
 exports.updatePost = (req, res, next) => {
     let form = new formidable.IncomingForm();
@@ -182,4 +172,47 @@ exports.unlike = (req, res) => {
             res.json(result);
         }
     });
+};
+
+exports.comment = (req, res) => {
+    let comment = req.body.comment;
+    comment.postedBy = req.body.userId;
+
+    Post.findByIdAndUpdate(
+        req.body.postId,
+        { $push: { comments: comment } },
+        { new: true }
+    )
+        .populate("comments.postedBy", "_id name")
+        .populate("postedBy", "_id name")
+        .exec((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.json(result);
+            }
+        });
+};
+
+exports.uncomment = (req, res) => {
+    let comment = req.body.comment;
+
+    Post.findByIdAndUpdate(
+        req.body.postId,
+        { $pull: { comments: { _id: comment._id } } },
+        { new: true }
+    )
+        .populate("comments.postedBy", "_id name")
+        .populate("postedBy", "_id name")
+        .exec((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.json(result);
+            }
+        });
 };
