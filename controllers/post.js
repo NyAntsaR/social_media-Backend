13b3fa677609c03a158af91754decb50b0c506aa
andy ratsirarson <andy.ratsirarson@andys-air.lan>
2019-12-03 16:22:46 -0800

    Handle form data when update post

--- controllers/post.js
+++ controllers/post.js
@@ -21,8 +21,7 @@ exports.getPosts = (req, res) => {
     const posts = Post.find()
         .populate("postedBy", "_id name")
         .select("_id title body created")
-        // sort based on the date
-        .sort({created: -1 })
+        .sort({ created: -1 })
         .then(posts => {
             res.json(posts);
         })
@@ -85,17 +84,47 @@ exports.isPoster = (req, res, next) => {
     next();
 };
 
+// exports.updatePost = (req, res, next) => {
+//     let post = req.post;
+//     post = _.extend(post, req.body);
+//     post.updated = Date.now();
+//     post.save(err => {
+//         if (err) {
+//             return res.status(400).json({
+//                 error: err
+//             });
+//         }
+//         res.json(post);
+//     });
+// };
+
 exports.updatePost = (req, res, next) => {
-    let post = req.post;
-    post = _.extend(post, req.body);
-    post.updated = Date.now();
-    post.save(err => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
         if (err) {
             return res.status(400).json({
-                error: err
+                error: "Photo could not be uploaded"
             });
         }
-        res.json(post);
+        // save post
+        let post = req.post;
+        post = _.extend(post, fields);
+        post.updated = Date.now();
+
+        if (files.photo) {
+            post.photo.data = fs.readFileSync(files.photo.path);
+            post.photo.contentType = files.photo.type;
+        }
+
+        post.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            res.json(post);
+        });
     });
 };
 
@@ -116,7 +145,7 @@ exports.deletePost = (req, res) => {
 exports.photo = (req, res, next) => {
     res.set("Content-Type", req.post.photo.contentType);
     return res.send(req.post.photo.data);
-}
+};
 
 exports.singlePost = (req, res) => {
     return res.json(req.post);
