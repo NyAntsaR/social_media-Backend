0c3af91fbcc2c3657621f0ee80237a9ccfb1e1fa
andy ratsirarson <andy.ratsirarson@andys-air.lan>
2019-12-05 15:18:27 -0800

    add api to routes

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 
 // apiDocs
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     fs.readFile("docs/apiDocs.json", (err, data ) => {
         if (err) {
             return res.status(400).json({
@@ -53,9 +53,9 @@ app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(expressValidator());
-app.use("/", postRoutes);
-app.use("/", authRoutes);
-app.use("/", userRoutes);
+app.use("/api", postRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 // middleware to handle unauthorized error using express-jwt
 app.use(function(err, req, res, next) {
     if (err.name === "UnauthorizedError") {
