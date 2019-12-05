c6ebefc4e3aa221fd441d76eee9cad1667a60f67
andy ratsirarson <andy.ratsirarson@andys-air.lan>
2019-12-05 13:51:34 -0800

    login with google

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { signup, signin, signout } = require("../controllers/auth");
+const { signup, signin, signout, socialLogin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 const { userSignupValidator } = require("../validator");
 
@@ -11,5 +11,7 @@ router.get("/signout", signout);
 
 // any route containing :userId, our app will first execute userByID()
 router.param("userId", userById);
+
+router.post("/social-login", socialLogin)
 
 module.exports = router;
